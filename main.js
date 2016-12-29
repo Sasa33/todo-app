c59381a00d56679c3db2bfc9535fccf2a52fb9b1
Sasa33 <chengsasa33@gmail.com>
2016-12-29 22:34:00 +0800

    sasa: add electron-reload to enable live reload for electron app

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
 
 const { app, BrowserWindow } = require('electron')
 
+require('electron-reload')(__dirname, {
+  electron: require('electron-prebuilt')
+});
+
 let mainWindow
 
 app.on('ready', () => {
   mainWindow = new BrowserWindow({width: 800, height: 600})
   mainWindow.loadURL(`file://${__dirname}/public/index.html`)
 
-  mainWindow.webContents.openDevTools()
+  // mainWindow.webContents.openDevTools()
 
   mainWindow.on('closed', function () {
     mainWindow = null
