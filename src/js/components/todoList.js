06041beb5d49d798415c974aedcc01ea4bd27fe5
Sasa33 <chengsasa33@gmail.com>
2017-02-04 16:59:10 +0800

    sasa: add filter types and filter todos

--- src/js/components/todoList.js
+++ src/js/components/todoList.js
@@ -2,17 +2,34 @@
 
 import React, { Component } from 'react';
 import Todo from '../components/todo'
+import { SHOW_ALL, SHOW_COMPLETED, SHOW_ACTIVE } from '../constants/FilterTypes'
+
+
+const filters = {
+  [SHOW_ALL]: () => true,
+  [SHOW_COMPLETED]: todo => todo.status === 'completed',
+  [SHOW_ACTIVE]: todo => todo.status === 'active',
+}
 
 class TodoList extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      filter: SHOW_ALL
+    }
+  }
+
   render() {
     const { todos, deleteTodo } = this.props
+    const { filter } = this.state
+    const filteredTodos = todos.filter(filters[filter])
 
     return (
       <div className="main">
         <input className="toggle-all" type="checkbox" />
         <ul className="todo-list">
           {
-            todos.map((todo, index) => <Todo key={index} todo={todo} deleteTodo= {deleteTodo} />)
+            filteredTodos.map((todo, index) => <Todo key={index} todo={todo} deleteTodo= {deleteTodo} />)
           }
         </ul>
       </div>
