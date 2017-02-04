96a005fb1cef37d120689b9d4f73f4495b6e2a00
Sasa33 <chengsasa33@gmail.com>
2017-02-04 17:53:55 +0800

    sasa: add class for completed todo

--- src/js/components/todo.js
+++ src/js/components/todo.js
@@ -2,10 +2,12 @@ import React from 'react'
 
 const Todo = ({todo, deleteTodo}) => {
   const {id, status, text} = todo
+  const completed = status === 'completed'
+
   return (
-    <li className="" id={id}>
+    <li className={completed ? 'completed' : ''} id={id}>
       <div className="view">
-        <input className="toggle" type="checkbox" />
+        <input className="toggle" type="checkbox" checked={completed} />
         <label>{text}</label>
         <button className="destroy" onClick={() => {deleteTodo(id)}}></button>
       </div>
